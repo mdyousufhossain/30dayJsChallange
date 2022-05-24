56aa4e2cfc23f82680fb2b54520cecad104f5eae
mdyousufhossain <yousafhossain3@gmail.com>
2022-05-24 15:46:58 +0600

    dudeitsworking

--- DayFour/04 - Array Cardio Day 1/cardio.js
+++ DayFour/04 - Array Cardio Day 1/cardio.js
@@ -2,7 +2,7 @@
     // Get your shorts on - this is an array workout!
     // ## Array Cardio Day 1
 
-    // Some data we can work with
+   
 
     const inventors = [
       { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
